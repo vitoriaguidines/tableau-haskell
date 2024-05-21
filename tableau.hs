4f50ee0a817d5d74294bca7409ea68baa038e526
Vitória Guidine <78456655+vitoriaguidines@users.noreply.github.com>
2024-05-21 12:50:11 -0300

    negação de arvore

--- tableau.hs
+++ tableau.hs
@@ -8,44 +8,55 @@ removeInconveniences :: String -> String
 removeInconveniences = filter (`notElem` [' ', '-'])
 
 -- Função para aplicar regras de prova e construir a árvore
-applyRules :: (Bool, Expr) -> [(Bool, Expr)]
-applyRules (True, Atom _) = []
-applyRules (False, Atom _) = []
-applyRules (v, Not f) = [(not v, f)]
-applyRules (True, And f1 f2) = [(True, f1), (True, f2)]
-applyRules (False, And f1 f2) = [(False, f1), (False, f2)]
-applyRules (True, Or f1 f2) = [(True, f1), (True, f2)]
-applyRules (False, Or f1 f2) = [(False, f1), (False, f2)]
-applyRules (True, Imply f1 f2) = [(False, f1), (True, f2)]
-applyRules (False, Imply f1 f2) = [(True, f1), (False, f2)]
+applyRules :: (Bool, Expr) -> [[(Bool, Expr)]]
+applyRules (True, Atom _) = [[]]
+applyRules (False, Atom _) = [[]]
+applyRules (v, Not f) = [[(not v, f)]]
+applyRules (True, And f1 f2) = [[(True, f1), (True, f2)]]
+applyRules (False, And f1 f2) = [[(False, f1)], [(False, f2)]]
+applyRules (True, Or f1 f2) = [[(True, f1)], [(True, f2)]]
+applyRules (False, Or f1 f2) = [[(False, f1), (False, f2)]]
+applyRules (True, Imply f1 f2) = [[(False, f1)], [(True, f2)]]
+applyRules (False, Imply f1 f2) = [[(True, f1), (False, f2)]]
 
 -- Função para construir a árvore de prova recursivamente
 buildProofTree :: [(Bool, Expr)] -> Tree (Bool, Expr)
 buildProofTree [] = Node (True, Atom ' ') []
 buildProofTree ((v, f):fs) =
-  let newFormulas = applyRules (v, f) ++ fs
-      branches = case applyRules (v, f) of
-                   [] -> [newFormulas]
-                   [x] -> [newFormulas]
-                   xs -> map (\x -> nub (x:fs)) xs
-  in Node (v, f) (map buildProofTree (filter (not . null) branches))
-
+  let branches = applyRules (v, f)
+  in Node (v, f) (map (buildProofTree . (++ fs)) branches)
 
 -- Função principal para construir a árvore de prova a partir de uma fórmula
 proofTree :: Expr -> Tree (Bool, Expr)
-proofTree formula = buildProofTree [(True, formula)]
+proofTree formula = buildProofTree [(False, formula)]
 
+-- Verifica se uma árvore de refutação é válida
+isValidRefutationTree :: Tree (Bool, Expr) -> Bool
+isValidRefutationTree (Node (val, expr) children)
+  | null children = not val && isAtomic expr
+  | val = False
+  | otherwise = all isValidRefutationTree children
+
+-- Verifica se uma expressão é um átomo
+isAtomic :: Expr -> Bool
+isAtomic (Atom _) = True
+isAtomic _ = False
+
+-- Função principal
 main :: IO ()
 main = do
     putStrLn "Insira uma fórmula lógica: "
     formula <- getLine
     let formulaWithoutInconveniences = removeInconveniences formula
     let parsedFormula = parse exprParser "" formulaWithoutInconveniences
-    
+
     case parsedFormula of
-        Left _ -> putStrLn "Fórmula inválida"
+        Left _ -> error "Invalid formula"
         Right expr -> do
             let tree = proofTree expr
             putStrLn $ drawTree $ fmap showNode tree
+            if isValidRefutationTree tree
+                then putStrLn "A árvore de refutação é válida."
+                else putStrLn "A árvore de refutação não é válida."
   where
     showNode (v, f) = (if v then "v: " else "f: ") ++ show f
