ac4ce3f0d74250d4d916db5f34528d2057c13f74
Lucas Serrano <lucas_serrano@id.uff.br>
2024-05-23 12:46:46 -0300

    verificação dos ramos feita

--- tableau.hs
+++ tableau.hs
@@ -8,6 +8,7 @@ import Data.List (nub)
 removeInconveniences :: String -> String
 removeInconveniences = filter (`notElem` [' ', '-'])
 
+
 -- Função para aplicar regras de prova e construir a árvore
 applyRules :: (Bool, Expr) -> [(Bool, Expr)]
 applyRules (True, Atom _) = []
@@ -20,12 +21,14 @@ applyRules (False, Or f1 f2) = [(False, f1), (False, f2)]
 applyRules (True, Imply f1 f2) = [(False, f1), (True, f2)]
 applyRules (False, Imply f1 f2) = [(True, f1), (False, f2)]
 
+
 -- Função para aplicar regras de prova com ramificação
 applyRulesWithBranching :: (Bool, Expr) -> (Bool, [(Bool, Expr)])
 applyRulesWithBranching (False, Imply f1 f2) = (False, [(True, f1), (False, f2)])
 applyRulesWithBranching (True, And f1 f2) = (False, [(True, f1), (True, f2)])
 applyRulesWithBranching (False, Or f1 f2) = (False, [(False, f1), (False, f2)])
 applyRulesWithBranching (v, f) = (True, applyRules (v, f))
+
 
 -- Função para construir a árvore de prova a partir de uma lista de fórmulas
 buildProofTree :: [(Bool, Expr)] -> Tree (Bool, Expr)
@@ -44,26 +47,56 @@ proofTree :: Expr -> Tree (Bool, Expr)
 proofTree formula = buildProofTree [(True, Not formula)]
 
 
-printTreeAsLists :: Tree (Bool, Expr) -> [[(Bool, Expr)]]
-printTreeAsLists (Node (v, f) []) = [[(v, f)]]
-printTreeAsLists (Node (v, f) branches) = 
-  map ((v, f) :) $ concatMap printTreeAsLists branches
+--Função para transformar cada caminho da árvore em uma lista
+branchTreeAsLists :: Tree (Bool, Expr) -> [[(Bool, Expr)]]
+branchTreeAsLists (Node (v, f) []) = [[(v, f)]]
+branchTreeAsLists (Node (v, f) branches) = map ((v, f) :) $ concatMap branchTreeAsLists branches
+
+
+-- Função para verificar se uma fórmula é válida
+checkValidate :: [[(Bool, Expr)]] -> IO ()
+checkValidate branches = do
+  let numberedBranches = zip [1..] branches
+  allClosed <- mapM printBranch numberedBranches
+  if and allClosed
+    then putStrLn "The function is valid"
+    else putStrLn "The function is not valid"
+  where
+    printBranch (branchNum, branch) = do
+      putStrLn $ "Branch " ++ show branchNum ++ ":"
+      let atoms = [(if v then "V: " else "F: ") ++ show f | (v, Atom f) <- branch]
+      putStrLn $ "Atoms: " ++ show atoms
+      let closed = checkContradiction atoms
+      if closed
+        then putStrLn "The branch is closed"
+        else putStrLn "The branch is open"
+      putStrLn ""
+      return closed
+  
+-- Função para verificar se uma lista de átomos contém uma contradição
+checkContradiction :: [String] -> Bool
+checkContradiction atoms = any checkAtom ['a' .. 'z']
+  where
+    checkAtom a = elem ("V: '" ++ [a] ++ "'") atoms && elem ("F: '" ++ [a] ++ "'") atoms
 
 main :: IO ()
 main = do
+
+  --b>(a&(b|a)) Inválida
+  --a>(a>(b>a)) Válida
+  --(p|(q&r))>((p|q)&(p|r)) Válida
     putStrLn "Insira uma fórmula lógica: "
     formula <- getLine
     let formulaWithoutInconveniences = removeInconveniences formula
     let parsedFormula = parse exprParser "" formulaWithoutInconveniences
-    
+
     case parsedFormula of
         Left _ -> putStrLn "Fórmula inválida"
         Right expr -> do
             let tree = proofTree expr
             putStrLn $ drawTree $ fmap showNode tree
-            let treeAsLists = printTreeAsLists tree
-            mapM_ print treeAsLists
 
-    
+            let treeAsLists = branchTreeAsLists tree
+            checkValidate treeAsLists
   where
     showNode (v, f) = (if v then "V: " else "F: ") ++ show f
